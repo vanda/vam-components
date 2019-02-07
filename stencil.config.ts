1a68e7b621efa4445d3c9eacc2889fa2969f705a
methodog <sittingbottom@gmail.com>
2019-02-07 17:21:53 +0000

    wip - shift css imports

--- stencil.config.ts
+++ stencil.config.ts
@@ -12,7 +12,9 @@ export const config: Config = {
         'node_modules/vam-fractal/src/components/mixins/breakpoints/_breakpoints.scss',
         'node_modules/vam-fractal/src/components/base/reset/_reset.scss',
         'node_modules/vam-fractal/src/components/base/site-color/_site-color.scss',
-        'node_modules/vam-fractal/src/components/base/typography/_typography.scss'
+        'node_modules/vam-fractal/src/components/base/typography/_typography.scss',
+        'node_modules/vam-fractal/src/components/units/button/_button.scss',
+        'node_modules/vam-fractal/src/components/units/link/_link.scss'
       ]
     })
   ],
