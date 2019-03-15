08e7cce6a5bca560e5f7b43f4b25db39b0276949
methodog <sittingbottom@gmail.com>
2019-03-15 12:36:57 +0000

    wip: try getting IE11 support with webcomponents-loader.js

--- babel.config.js
+++ babel.config.js
@@ -5,7 +5,8 @@ module.exports = {
         [
           '@babel/preset-env',
           {
-            modules: false
+            modules: false,
+            useBuiltIns: 'usage'
           }
         ]
       ],
